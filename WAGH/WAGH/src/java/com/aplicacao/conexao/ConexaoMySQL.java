8fe6d602b96ffae646a1d76dfe47e44d8a135dbf
Victor Afonso <102774818+victorafonsofr@users.noreply.github.com>
2024-10-21 15:37:06 -0300

    Add files via upload

--- WAGH/WAGH/src/java/com/aplicacao/conexao/ConexaoMySQL.java
+++ WAGH/WAGH/src/java/com/aplicacao/conexao/ConexaoMySQL.java
@@ -12,7 +12,7 @@ public class ConexaoMySQL {
             Class.forName("com.mysql.cj.jdbc.Driver"); // driver
             String ip = "127.0.0.1"; // IP do servidor de banco
             String us = "root"; // usuário do banco
-            String bd = "clientes1"; // nome do banco de dados
+            String bd = "wagh"; // nome do banco de dados
             String pw = ""; // senha do usuário
             con =  DriverManager.getConnection("jdbc:mysql://" + ip + 
                  "/" + bd,us,pw); // instrução para conectar com o BD
